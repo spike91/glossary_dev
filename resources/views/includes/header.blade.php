3f2161b72c9e461a35705c7a21322f658d9261b8
spike91 <emlecap7@mail.ru>
2018-01-07 14:36:32 +0200

    delete

--- resources/views/includes/header.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container ">
-    <div class="row">
-            <div class="col-md-12">
-                <div class="header">
-                    <a href="/"><img src="/images/logoo.png" width="200" alt="picture"></a>                
-                </div>
-            </div>
-    </div>
-</div>
