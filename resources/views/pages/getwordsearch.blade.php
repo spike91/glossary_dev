2e63a11bc68749ff508357eee30b78a4ca86d333
spike91 <ivanpanas91@gmail.com>
2018-01-07 14:22:11 +0200

    Revert "mir"

--- resources/views/pages/getwordsearch.blade.php
+++ resources/views/pages/getwordsearch.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <?php $language = strtolower(LaravelLocalization::getCurrentLocaleName());
-$categoryLanguage = 'category'.$language;
+$categoryLanguage = $language.'_category';
 ?>
 @extends('layouts.app')
 
@@ -12,8 +12,8 @@ $categoryLanguage = 'category'.$language;
         <tbody>
             @foreach ($words as $w)
                 <tr>
-                    <td><a href="{{ LaravelLocalization::getLocalizedURL(null, 'word/'.$w->id.'/'.$w->categoryID, [], true) }}"
-                    class="list-group-item list-group-item-action">{{$w->$language}} ({{$w->categoryLanguage}})</a></td>
+                    <td><a href="{{ LaravelLocalization::getLocalizedURL(null, 'category='.$w->id_category.'/word='.$w->id, [], true) }}"
+                    class="list-group-item list-group-item-action">{{$w->$language}} ( {{($w->$categoryLanguage) }})</a></td>
                 </tr>
             @endforeach
         </tbody>
