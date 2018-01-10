6114b014ee2bc7935f4b65ecab5f179ccb4e4574
spike91 <emlecap7@mail.ru>
2018-01-11 00:47:42 +0200

    fix edit category form

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-{!! Form::open(['url'=> LaravelLocalization::getLocalizedURL(null, '/', [], true), 'method' => 'get', 'id' => 'category-add-form']) !!}<!-- Ссылка, куда отправить после нажатия на добавить-->
+{!! Form::open(['url'=> LaravelLocalization::getLocalizedURL(null, 'category/save/'.$category->id, [], true), 'method' => 'get', 'id' => 'category-add-form']) !!}<!-- Ссылка, куда отправить после нажатия на добавить-->
     <div class="form-group col-md-6 col-md-offset-4">
       {!! Form::label('russian', 'Название категории на русском:') !!}
-      {!! Form::text('russian', null, ['class'=>'form-control','maxlength' => 255,'required']) !!}
+      {!! Form::text('russian', $category->russian, ['class'=>'form-control','maxlength' => 60,'required']) !!}
       {!! Form::label('english', 'Category name in English:') !!}
-      {!! Form::text('english', null, ['class'=>'form-control','maxlength' => 255,'required']) !!}
+      {!! Form::text('english', $category->english, ['class'=>'form-control','maxlength' => 60,'required']) !!}
       {!! Form::label('estonian', 'Kategooria nimi eesti keeles:') !!}
-      {!! Form::text('estonian', null, ['class'=>'form-control','maxlength' => 255,'required']) !!}
+      {!! Form::text('estonian', $category->estonian, ['class'=>'form-control','maxlength' => 60,'required']) !!}
     </div>
     <div class="form-group col-md-6 col-md-offset-4">
       {!! Form::submit(__('Edit category'), ['class'=>'btn btn-info form-control']) !!}
