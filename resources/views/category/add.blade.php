c0499201c2ca23af8fbececbb0f5349c3d3a61fc
spike91 <emlecap7@mail.ru>
2018-01-11 00:51:09 +0200

    updaded form input maxlength => 60

--- resources/views/category/add.blade.php
+++ resources/views/category/add.blade.php
@@ -4,11 +4,11 @@
    {!! Form::open(['url'=> LaravelLocalization::getLocalizedURL(null, '/category/save', [], true), 'method' => 'get', 'id' => 'category-add-form']) !!}<!-- Ссылка, куда отправить после нажатия на добавить-->
     <div class="form-group col-md-6 col-md-offset-4">
       {!! Form::label('russian', 'Название категории на русском:') !!}
-      {!! Form::text('russian', null, ['class'=>'form-control','maxlength' => 255,'required']) !!}
+      {!! Form::text('russian', null, ['class'=>'form-control','maxlength' => 60,'required']) !!}
       {!! Form::label('english', 'Category name in English:') !!}
-      {!! Form::text('english', null, ['class'=>'form-control','maxlength' => 255,'required']) !!}
+      {!! Form::text('english', null, ['class'=>'form-control','maxlength' => 60,'required']) !!}
       {!! Form::label('estonian', 'Kategooria nimi eesti keeles:') !!}
-      {!! Form::text('estonian', null, ['class'=>'form-control','maxlength' => 255,'required']) !!}
+      {!! Form::text('estonian', null, ['class'=>'form-control','maxlength' => 60,'required']) !!}
     </div>
     <div class="form-group col-md-6 col-md-offset-4">
       {!! Form::submit(__('Add category'), ['class'=>'btn btn-info form-control']) !!}
