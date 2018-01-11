54f31a84b92d389541d1f7fff6a967f79e3175cc
spike91 <emlecap7@mail.ru>
2018-01-11 04:00:03 +0200

    update form for two routes

--- resources/views/word/add.blade.php
+++ resources/views/word/add.blade.php
@@ -1,38 +1,45 @@
 @extends('layouts.app')
 <?php $categoryName = strtolower(LaravelLocalization::getCurrentLocaleName())?>
 @section('content')
-<H3 align="center">@lang('sidebar.new')</H3>
+<H3 align="center">{{ $description==null ? __('sidebar.new'):__('Editing Word')}}</H3>
 <hr/>
-{!! Form::open(['url'=> LaravelLocalization::getLocalizedURL(null, '/word/save', [], true), 'method' => 'get', 'id' => 'word-add-form']) !!}<!-- Ссылка, куда отправить после нажатия на добавить-->
+{!! Form::open(['url'=> LaravelLocalization::getLocalizedURL(null, $description==null ? '/word/save':'/word/update', [], true), 'method' => 'get', 'id' => 'word-add-form']) !!}
     <div class="form-group col-md-5 col-md-offset-7">
+    {{ Form::hidden('wordId', $word==null ? null:$word->id) }}
       {!! Form::label('word_russian', 'Слово на русском:') !!}
-      {!! Form::text('word_russian', null, ['class'=>'form-control', 'maxlength' => 255,'required']) !!}
+      {!! Form::text('word_russian', $word==null ? null:$word->russian, ['class'=>'form-control', 'maxlength' => 60,'required']) !!}
       {!! Form::label('word_english', 'Words in english:') !!}
-      {!! Form::text('word_english', null, ['class'=>'form-control', 'maxlength' => 255,'required']) !!}
+      {!! Form::text('word_english', $word==null ? null:$word->english, ['class'=>'form-control', 'maxlength' => 60,'required']) !!}
       {!! Form::label('word_estonian', 'Sõna eesti keeles:') !!}
-      {!! Form::text('word_estonian', null, ['class'=>'form-control', 'maxlength' => 255,'required']) !!}
+      {!! Form::text('word_estonian', $word==null ? null:$word->estonian, ['class'=>'form-control', 'maxlength' => 60,'required']) !!}
     </div>
     <div class="form-group col-md-8 col-md-offset-4">
+    {{ Form::hidden('descId', $description==null ? null:$description->id) }}
     {!! Form::label('desc_russian', 'Описание на русском:') !!}
-    {!! Form::textarea('desc_russian', null, ['class'=>'form-control','style'=>'height:100px','maxlength' => 255,'required']) !!}
+    {!! Form::textarea('desc_russian', $description==null ? null:$description->russian, ['class'=>'form-control','style'=>'height:100px','maxlength' => 255,'required']) !!}
     {!! Form::label('desc_english', 'Description in english:') !!}
-    {!! Form::textarea('desc_english', null, ['class'=>'form-control','style'=>'height:100px','maxlength' => 255,'required']) !!}
+    {!! Form::textarea('desc_english', $description==null ? null:$description->english, ['class'=>'form-control','style'=>'height:100px','maxlength' => 255,'required']) !!}
     {!! Form::label('desc_estonian', 'Seletus eesti keeles:') !!}
-    {!! Form::textarea('desc_estonian', null, ['class'=>'form-control','style'=>'height:100px', 'maxlength' => 255,'required']) !!}
+    {!! Form::textarea('desc_estonian', $description==null ? null:$description->estonian, ['class'=>'form-control','style'=>'height:100px', 'maxlength' => 255,'required']) !!}
     </div>
     <div class="form-group col-md-8 col-md-offset-4">
       {!! Form::label('image', 'Link:') !!}
-      {!! Form::text('image', null, ['class'=>'form-control', 'maxlength' => 255]) !!}
+      {!! Form::text('image', $description==null ? null:$description->image, ['class'=>'form-control', 'maxlength' => 255]) !!}
     </div>
      <div class="form-group col-md-6 col-md-offset-3">     
-     <select class="form-control" name="category" require>
+     <select class="form-control" name="category" require> 
+          <?php $category_fk = $description==null ? null:$description->category_fk ?>    
         @foreach($categories as $category)
-          <option value="{{ $category->id }}">{{ $category->$categoryName }}</option>
+          @if($category->id == $category_fk)
+          <option value="{{ $category->id }}" selected>{{ $category->$categoryName }}</option>
+          @else
+            <option value="{{ $category->id }}">{{ $category->$categoryName }}</option>
+          @endif        
         @endforeach
       </select>
     </div> 
     <div class="form-group col-md-8 col-md-offset-4">
-      {!! Form::submit(__('Add Word'), ['class'=>'btn btn-info form-control']) !!}
+      {!! Form::submit($description==null ? __('Add Word'):__('Edit Word'), ['class'=>'btn btn-info form-control']) !!}
     </div>
   {!! Form::close() !!}
 @endsection
